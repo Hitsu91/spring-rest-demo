899308a6e60a1e528c5d3a52722fd66342751a63
Hitsu91 <lucafang@hotmail.it>
2021-02-11 16:34:47 +0100

    initial commit

---
+++ src/test/java/con/antsrl/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package con.antsrl.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
